200749b900868d513c36e51b2d6c861d3b90eeaf
AidanJones14 <115672129+AidanJones14@users.noreply.github.com>
2026-06-12 13:41:42 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int minIndex = 0;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < min) {
+                min = prices[i];
+            } else{
+                int diff = prices[i] - min;
+                profit =  diff > profit ? diff : profit;
+            }
+        }
+        return profit;
+    }
+}
